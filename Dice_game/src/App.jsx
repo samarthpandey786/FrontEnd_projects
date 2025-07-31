5e25d4ec44c9ef396184aba1a10c6718024637bc
samarth pandey <samarthpandey676@gmail.com>
2025-07-31 21:49:39 +0530

    Initialize Dice Game project with React, Vite, and Tailwind CSS
    - Created package.json with dependencies and scripts for development
    - Added PostCSS configuration for Tailwind CSS and Autoprefixer
    - Included Vite logo SVG in the public directory
    - Created App component with initial layout and styling
    - Added React logo SVG in assets
    - Set up global CSS file with Tailwind directives
    - Implemented main entry point for the application
    - Configured Tailwind CSS with content paths
    - Established Vite configuration for the project

---
+++ Dice_game/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    
+    <div className='w-full h-screen bg-gray-200 flex   justify-center items-start mt-4 flex-row gap-6'>
+      <div className='flex flex-col basis-1/2 justify-center items-center gap-6 bg-emerald-500 p-6 rounded-lg w-[25%] h-[25%]'>
+        <div className='flex flex-col justify-center items-center gap-6 bg-black p-6 rounded-lg w-[100%] h-[100%]'>
+
+        </div>
+
+      </div>
+      <div className='flex flex-col basis-1/2 justify-center items-center gap-6 bg-emerald-500 p-6 rounded-lg w-[25%] h-[25%]'>
+        <div className='flex flex-col justify-center items-center gap-6 bg-black p-6 rounded-lg w-[100%] h-[100%]'>
+
+        </div>
+
+      </div>
+      <div className='flex flex-col basis-2/2 justify-center items-center gap-6 bg-emerald-500 p-6 rounded-lg w-[25%] h-[25%]'>
+        <div className='flex flex-col justify-center items-center gap-6 bg-black p-6 rounded-lg w-[100%] h-[100%]'>
+
+        </div>
+
+      </div>
+      <div className='flex flex-col basis-1/2 justify-center items-center gap-6 bg-emerald-500 p-6 rounded-lg w-[25%] h-[25%]'>
+        <div className='flex flex-col justify-center items-center gap-6 bg-black p-6 rounded-lg w-[100%] h-[100%]'>
+
+        </div>
+
+      </div>
+      <div className='flex flex-col justify-center gap-6 items-center bg-black basis-3/5 p-6 rounded-lg w-[25%] h-[25%]'>
+        <div className='flex flex-col  justify-center items-center gap-6 bg-emerald-500 p-6 rounded-lg w-[100%] h-[100%]'>
+
+        </div>
+      </div>
+      </div>
+      
+    
+  )
+}
+
+export default App
